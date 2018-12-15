bc17d476b8f849ea7432ba9f387ed12969b94809
Angelini <llouis@yahoo.com>
2018-12-15 14:32:14 +0000

    Show action sheet

--- src/pages/response/response.ts
+++ src/pages/response/response.ts
@@ -6,11 +6,17 @@ import {
     NavParams
 } from 'ionic-angular';
 import {
-    HttpClient, HttpHeaders
+    HttpClient,
+    HttpHeaders
 } from '@angular/common/http';
 import {
     Storage
 } from '@ionic/storage';
+
+import {
+    ActionSheetController
+} from 'ionic-angular';
+import { hasLifecycleHook } from '@angular/compiler/src/lifecycle_reflector';
 
 @Component({
     selector: 'Response',
@@ -20,19 +26,22 @@ export class Response {
     token = null;
     httpOptions = null
     invitations = null
-
-    constructor(public navCtrl: NavController, public navParams: NavParams, public httpClient: HttpClient, private storage: Storage) {
+    presentActionSheet = null
+    constructor(public navCtrl: NavController, public navParams: NavParams, public httpClient: HttpClient, private storage: Storage,
+        public actionSheetCtrl: ActionSheetController) {
         // If we navigated to this page, we will have an item available as a nav param
+
+        
         
         this.storage.get('token').then(data => {
             this.token = data
             this.httpOptions = {
                 headers: new HttpHeaders({
-                  'Content-Type':  'application/json',
-                  'Authorization': "Bearer " + this.token
+                    'Content-Type': 'application/json',
+                    'Authorization': "Bearer " + this.token
                 })
-              };
-            this.httpClient.get('http://localhost:5000/invitations/user/1', this.httpOptions ).subscribe(data => {
+            };
+            this.httpClient.get('http://localhost:5000/invitations/user/1', this.httpOptions).subscribe(data => {
                 console.log(data)
                 this.invitations = data
 
@@ -42,6 +51,31 @@ export class Response {
     }
 
 
-
+    hello(){
+        console.log( this.actionSheetCtrl.create )
+        this.actionSheetCtrl.create({
+            title: 'Modify your album',
+            buttons: [
+              {
+                text: 'Destructive',
+                role: 'destructive',
+                handler: () => {
+                  console.log('Destructive clicked');
+                }
+              },{
+                text: 'Archive',
+                handler: () => {
+                  console.log('Archive clicked');
+                }
+              },{
+                text: 'Cancel',
+                role: 'cancel',
+                handler: () => {
+                  console.log('Cancel clicked');
+                }
+              }
+            ]
+          }).present()
+    }
 
 }
