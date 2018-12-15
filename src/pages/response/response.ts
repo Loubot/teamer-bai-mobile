e88874d200f7dd6d22a43c3a7bade780827d9c97
Angelini <llouis@yahoo.com>
2018-12-15 12:07:15 +0000

    Highlight confirmed in colour

--- src/pages/response/response.ts
+++ src/pages/response/response.ts
@@ -19,7 +19,7 @@ import {
 export class Response {
     token = null;
     httpOptions = null
-    user = {}
+    invitations = null
 
     constructor(public navCtrl: NavController, public navParams: NavParams, public httpClient: HttpClient, private storage: Storage) {
         // If we navigated to this page, we will have an item available as a nav param
@@ -32,9 +32,9 @@ export class Response {
                   'Authorization': "Bearer " + this.token
                 })
               };
-            this.httpClient.get('http://localhost:5000/event/user/1', this.httpOptions ).subscribe(data => {
+            this.httpClient.get('http://localhost:5000/invitations/user/1', this.httpOptions ).subscribe(data => {
                 console.log(data)
-                this.user = data
+                this.invitations = data
 
             })
         })
