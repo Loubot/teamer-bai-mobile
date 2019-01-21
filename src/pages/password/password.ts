baf2721401e292f869e98a0e09993e2fcecd41ad
Angelini <Lllouis@yahoo.com>
2019-01-21 19:36:59 +0000

    Updating password

--- src/pages/password/password.ts
+++ src/pages/password/password.ts
@@ -1,7 +1,32 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import {FormGroup, FormBuilder, Validators} from '@angular/forms';
-import { TextEqualityValidatorModule } from "ngx-text-equality-validator";
+import {
+    Component
+} from '@angular/core';
+import {
+    IonicPage,
+    NavController,
+    NavParams
+} from 'ionic-angular';
+
+import {
+    HttpClient,
+    HttpHeaders
+} from '@angular/common/http';
+
+import {
+    FormGroup,
+    FormBuilder,
+    Validators
+} from '@angular/forms';
+import {
+    TextEqualityValidatorModule
+} from "ngx-text-equality-validator";
+import {
+    Storage
+} from '@ionic/storage';
+
+import {
+    HostnameProvider
+} from '../../providers/hostname/hostname'
 
 
 /**
@@ -13,27 +38,50 @@ import { TextEqualityValidatorModule } from "ngx-text-equality-validator";
 
 @IonicPage()
 @Component({
-  selector: 'page-password',
-  templateUrl: 'password.html',
+    selector: 'page-password',
+    templateUrl: 'password.html',
 })
 export class PasswordPage {
-  passwordForm: FormGroup
-  valid = false
-  constructor(public navCtrl: NavController, public navParams: NavParams, public fb: FormBuilder ) {
-    this.passwordForm = this.fb.group({
-      password: [""],
-      repeatPassword: [""]
-    });
-    console.log( this.passwordForm.dirty )
-  }
+    passwordForm: FormGroup
+    valid = false
+    user = null
+    token = null
+    httpOptions = null
+    constructor(public navCtrl: NavController, public navParams: NavParams, public fb: FormBuilder, private storage: Storage, public httpClient: HttpClient,
+                public global: HostnameProvider ) {
+        this.passwordForm = this.fb.group({
+            password: [""],
+            repeatPassword: [""]
+        });
+        this.storage.get('user_token').then(data => {
+            this.user = data.user
+            this.token = data.token
+        })
+    }
 
-  onSubmit() {
-    this.valid = ( this.passwordForm.value.password === this.passwordForm.value.repeatPassword )
-    console.log( this.passwordForm.dirty )
-  }
+    onSubmit() {
+        this.httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + this.token
+            })
+        };
+        this.valid = (this.passwordForm.value.password === this.passwordForm.value.repeatPassword)
+        if (this.valid) {
+            this.httpClient.put(this.global.hostname + 'update-password', {
+                email: this.user.email,
+                password: this.passwordForm.value.password
+            },
+            this.httpOptions).subscribe(data => {
+            console.log(data)
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad PasswordPage');
-  }
+        })
+        }
+        console.log(this.passwordForm.dirty)
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad PasswordPage');
+    }
 
 }
